455b68d2d3095b2226bc53fae1d6315389a981fa
Kamila Ojeda <beau.ojeda@gmail.com>
2023-05-07 19:07:40 -0300

    Validaciones de tipos de datos.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 app = FastAPI()
@@ -11,12 +11,26 @@ app.version = "0.0.1"
 class Movie(BaseModel):
     # id: int | None = None ## None es para indicar que puede ser opcional, sin embargo hay otra forma: tiping.
     id: Optional[int] = None
-    tittle: str
-    overview: str
-    year: int
-    rating: float
+    ## Se pueden agregar validaciones importando Field
+    tittle: str = Field(min_length=5, max_length=15)
+    overview: str = Field(min_length=15, max_length=50)
+    year: int = Field(le=2022)
+    rating: float = Field(..., ge=1, le=10) ## el menor es 1, ge= greater tahn o equal to, y el max es 10, le=less than or equal to. Los puntos suspensivos indican que el campo es obligatorio y no puede ser nulo
     category: str
 
+    ## Agregamos una clase de ejemplo de como deberia estar formado el objeto de cada película.
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "tittle": "Mi película",
+                "overview": "Sinopsis de la película",
+                "year": 2022,
+                "rating": 7.8,
+                "category": "Drama"
+            }
+        }
+        
 movies = [
     {
         "id": 1,
@@ -101,3 +115,7 @@ def update_movie(id: int):
         if item["id"] == id:
             movies.remove(item)
             return movies
+        
+# Validaciones de tipos de datos
+## importamos field desde pydantic
+## indicar en el esquema con la palabra Field
