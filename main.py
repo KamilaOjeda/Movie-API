c9b999a4048fed512565ca92010cb82a4970a287
Kamila Ojeda <beau.ojeda@gmail.com>
2023-05-07 18:27:33 -0300

    Esquemas con Pydantic.

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import Optional
+
 app = FastAPI()
 app.title = "Mi primera app con FastAPI"
 app.version = "0.0.1"
+
+# Creamos la clase/esquema que va a contener tod la info de cada película
+class Movie(BaseModel):
+    # id: int | None = None ## None es para indicar que puede ser opcional, sin embargo hay otra forma: tiping.
+    id: Optional[int] = None
+    tittle: str
+    overview: str
+    year: int
+    rating: float
+    category: str
 
 movies = [
     {
@@ -52,27 +65,33 @@ def get_movies_by_category(category: str, year:int):
 
 # Método POST
 @app.post("/movies", tags=["movies"])
-def create_movie(id: int = Body(), tittle: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
-    movies.append({ # Insertar datos
-        "id": id,
-        "tittle": tittle,
-        "overview": overview,
-        "year": year,
-        "rating": rating,
-        "category": category
-    })
+# def create_movie(id: int = Body(), tittle: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
+def create_movie(movie: Movie): ## En vez de poner cada elemento del body, utilizamos este atajo
+    movies.append(movie) # Insertar datos
     return movies
+
+# # Método PUT, como parámetro de ruta
+# @app.put("/movies/{id}", tags=["movies"])
+# def update_movie(id: int, tittle: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
+#     for item in movies:
+#         if item["id"] == id:
+#             item["tittle"] = tittle,
+#             item["overview"] = overview,
+#             item["year"] = year,
+#             item["rating"] = rating, 
+#             item["category"] = category
+#             return movies
 
 # Método PUT, como parámetro de ruta
 @app.put("/movies/{id}", tags=["movies"])
-def update_movie(id: int, tittle: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
+def update_movie(id: int, movie: Movie):
     for item in movies:
         if item["id"] == id:
-            item["tittle"] = tittle,
-            item["overview"] = overview,
-            item["year"] = year,
-            item["rating"] = rating, 
-            item["category"] = category
+            item["tittle"] = movie.tittle
+            item["overview"] = movie.overview
+            item["year"] = movie.year
+            item["rating"] = movie.rating
+            item["category"] = movie.category
             return movies
 
 # Método DELETE, como parámetro de ruta
