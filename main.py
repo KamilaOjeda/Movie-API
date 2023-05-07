049542b2068267c17d294ca569f327b2b92863cb
Kamila Ojeda <beau.ojeda@gmail.com>
2023-05-07 17:35:16 -0300

    Método GET.

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
-
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 app.title = "Mi primera app con FastAPI"
 app.version = "0.0.1"
 
-@app.get('/')
+movies = [
+    {
+        "id": 1,
+    "tittle": "Avatar",
+    "overview": "En un exhuberante planeta llamado Pandora, viven los Na'vi",
+    "year": "2009",
+    "rating": "7.8",
+    "category": "Acción"
+    }
+]
+
+@app.get('/', tags=["home"])
 def message():
-    return "Hello world!"
+    return HTMLResponse("<h1>Hello world<h1>")
+
+@app.get("/movies", tags=["movies"])
+def get_movies():
+    return movies
